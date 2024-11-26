a985005920d17c87bb0573dab653366adf3b9a93
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-11-26 13:07:23 +0000

    JavaScriptの基本文法

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-alert('Hello World');
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+ 
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+ 
+let i;
+let num1 = 0;
+
+for(i = 1; i < 11; i++){
+  num1 = num1 + i;
+}
+
+alert('1から10まで足し算した結果は' + num1 + 'です');
+ 
